8c5bb0e59cd66872485c9a3c0b40bb389f8524bf
Benjamin Glick <glick@lclark.edu>
2017-10-02 14:12:18 -0700

    add placeholders for necessary functions

---
+++ src/execution_interaction.py
@@ -0,0 +1,30 @@
+import docker
+import schedd
+
+
+def submit():
+    raise NotImplementedError
+
+
+def status():
+    raise NotImplementedError
+
+
+def cancel():
+    raise NotImplementedError
+
+
+def process_json():
+    raise NotImplementedError
+
+
+def prepare_execution_environment():
+    raise NotImplementedError
+
+
+def stage_inputs():
+    raise NotImplementedError
+
+
+def dockerize():
+    raise NotImplementedError
---
+++ web/app.py
@@ -0,0 +1,41 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+@app.route('/index.html')
+@app.route('/index.php')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/jobs')
+def status_page():
+    raise NotImplementedError
+
+
+@app.route('/login')
+def login():
+    raise NotImplementedError
+
+
+@app.route('/logout')
+def logout():
+    raise NotImplementedError
+
+
+@app.route('/authcallback')
+def authcallback():
+    raise NotImplementedError
+
+
+@app.route('/howto')
+@app.route('/help')
+def help():
+    raise NotImplementedError
+
+
+@app.route('/about')
+def about():
+    raise NotImplementedError
