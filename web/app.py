40bc237aeeeabf0b80c95dd50b752cff04ac3bcb
Benjamin Glick <glick@lclark.edu>
2017-10-08 16:26:52 -0700

    made many changes to web backend. Uploads are now processed correctly

--- web/app.py
+++ web/app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request, redirect
 import sqlite3
-from time import sleep
 from werkzeug import secure_filename
-
 import os
+import glob
+import uuid
 import json
 app = Flask(__name__)
 
 app.config["db_cursor"] = sqlite3.connect("interface.db").cursor()
+app.config['upload_base_dir'] = "/Users/ben/Google Drive/class/y2/ind_study/workspace/uploads/"
 
 
 @app.route('/')
@@ -67,18 +68,39 @@ def sanitize(string):
 
 def sanitize_for_filename(filename):
     keepcharacters = (' ', '.', '_')
-    return "".join(c for c in filename if c.isalnum() or c in keepcharacters).rstrip()
+    safe = "".join(c for c in filename if c.isalnum()
+                   or c in keepcharacters).rstrip()
+    return safe.replace(" ", "_")
 
 
 @app.route('/script', methods=["POST", "GET"])
 def script_handler():
-    jobname = dict(request.form).get('job_name', ["job"])[0]
-    filename = dict(request.form).get('filename', ['script'])[0]
-    fs = dict(request.files).get("filestructure")
+    jobname = sanitize_for_filename(
+        dict(request.form).get('job_name', ["job"])[0])
+    filename = sanitize_for_filename(
+        dict(request.form).get('filename', ['script'])[0])
+    fs = dict(request.files).get("filestructure")[0]
     script = dict(request.files).get("executable")[0]
-    print(request.form, "\n", script, "\n", fs)
-    script.save(sanitize_for_filename(filename))
+    job_uuid = uuid.uuid1()
+    jobname = make_base_dir(filename, jobname, script)
+    fs.save(app.config["upload_base_dir"] + jobname + ".json")
+    parse_filesystem(jobname)
     return redirect('/newjob')
+
+
+def make_base_dir(filename, jobname, script):
+    if not os.path.exists(app.config['upload_base_dir'] + jobname):
+        os.makedirs(app.config['upload_base_dir'] +
+                    sanitize_for_filename(jobname))
+    else:
+        jobname = jobname + \
+            len(glob.glob(app.config['upload_base_dir'] + jobname))
+    script.save(app.config['upload_base_dir'] + jobname + "/" + filename)
+    return jobname
+
+
+def parse_filesystem(jobname):
+    fs_desc = open((app.config["upload_base_dir"] + jobname + ".json", "r"))
 
 
 def authenticated(function):
