be6b270af27c657d8210d466176b05b3fc5cd648
Benjamin Glick <glick@lclark.edu>
2017-10-08 00:50:33 -0700

    add input sanitization functions to backend

--- web/app.py
+++ web/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import sqlite3
 from time import sleep
 from werkzeug import secure_filename
 
@@ -6,13 +7,22 @@ import os
 import json
 app = Flask(__name__)
 
+app.config["db_cursor"] = sqlite3.connect("interface.db").cursor()
+
 
 @app.route('/')
 @app.route('/index')
 @app.route('/index.html')
 @app.route('/index.php')
+@app.route('/home')
+@app.route('/home.html')
 def hello_world():
     return render_template("home.html")
+
+
+@app.route('/filesystem_generator')
+def fs_gen():
+    raise NotImplementedError
 
 
 @app.route('/jobs')
@@ -51,11 +61,23 @@ def submit_page():
     return render_template('submit.html')
 
 
+def sanitize(string):
+    string = string.decode("utf8", "ignore")
+
+
+def sanitize_for_filename(filename):
+    keepcharacters = (' ', '.', '_')
+    return "".join(c for c in filename if c.isalnum() or c in keepcharacters).rstrip()
+
+
 @app.route('/script', methods=["POST", "GET"])
 def script_handler():
-    jobname = dict(request.form).get('job_name', "job")
-    script = dict(request.files).get("executable")
-    print(request.form, "\n", script)
+    jobname = dict(request.form).get('job_name', ["job"])[0]
+    filename = dict(request.form).get('filename', ['script'])[0]
+    fs = dict(request.files).get("filestructure")
+    script = dict(request.files).get("executable")[0]
+    print(request.form, "\n", script, "\n", fs)
+    script.save(sanitize_for_filename(filename))
     return redirect('/newjob')
 
 
