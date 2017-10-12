0626636549f850b4a3487d526b1b8550c2001c8f
Benjamin Glick <glick@lclark.edu>
2017-10-11 17:44:39 -0700

    more refactoring

--- web/app.py
+++ web/app.py
@@ -3,7 +3,7 @@ import time
 import uuid
 import sqlite3
 import hashlib
-# from database_helpers import None
+from database_helpers import db_save_job, increment_job_counter
 from security_helpers import sanitize_for_filename, requires_auth
 from exec_helpers import parse_filesystem, make_job_base_dir
 
@@ -110,25 +110,6 @@ def all_jobs():
 def submit_page():
     print(session)
     return render_template('submit.html')
-
-
-def db_save_job(jn, fn, cli, u_uuid, desc, b_dir):
-    job_uuid = str(uuid.uuid1())
-    now = str(time.ctime())
-    app.config['db_cursor'].execute("""insert into jobs (job_name, cli_invoc, 
-    time_created, j_id, creator_uuid, status, base_dir,
-    exe_filename, size, desc) VALUES (?,?,?,?,?,?,?,?,?,?) """, (
-        jn, cli, now, job_uuid, u_uuid, "Pending", b_dir, fn, 1, desc)
-    )
-    app.config['db_conn'].commit()
-
-
-def increment_job_counter(user_id):
-    current = int(app.config['db_cursor'].execute(
-        'select jobs_executed from users where user_uuid=?', (user_id,)).fetchone()[0]) + 1
-    app.config['db_cursor'].execute(
-        "update users set jobs_executed=? where user_uuid=?", (current, user_id))
-    app.config['db_conn'].commit()
 
 
 @app.route('/script', methods=["POST"])
--- web/database_helpers.py
+++ web/database_helpers.py
@@ -1,1 +1,24 @@
 # helper functions for the database accesses
+import app
+import sqlite3
+import uuid
+import time
+
+
+def db_save_job(jn, fn, cli, u_uuid, desc, b_dir):
+    job_uuid = str(uuid.uuid1())
+    now = str(time.ctime())
+    app.app.config['db_cursor'].execute("""insert into jobs (job_name, cli_invoc, 
+    time_created, j_id, creator_uuid, status, base_dir,
+    exe_filename, size, desc) VALUES (?,?,?,?,?,?,?,?,?,?) """, (
+        jn, cli, now, job_uuid, u_uuid, "Pending", b_dir, fn, 1, desc)
+    )
+    app.app.config['db_conn'].commit()
+
+
+def increment_job_counter(user_id):
+    current = int(app.app.config['db_cursor'].execute(
+        'select jobs_executed from users where user_uuid=?', (user_id,)).fetchone()[0]) + 1
+    app.app.config['db_cursor'].execute(
+        "update users set jobs_executed=? where user_uuid=?", (current, user_id))
+    app.app.config['db_conn'].commit()
